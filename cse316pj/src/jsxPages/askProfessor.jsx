3deeae04d741653f46c8a0aeaabdcc0b09b93d26
JoohyoungJun <joohyoung.jun@stonybrook.edu>
2024-12-03 18:53:12 +0900

    joo: modified askProfessor

--- cse316pj/src/jsxPages/askProfessor.jsx
+++ cse316pj/src/jsxPages/askProfessor.jsx
@@ -19,7 +19,7 @@ const AskProfessor = () => {
     const fetchQuestions = async () => {
       try {
         const response = await axios.get('http://localhost:3001/questions');
-        setQuestions(response.data);
+        setQuestionList(response.data);
       } catch (error) {
         console.error('Error fetching questions:', error);
       }
